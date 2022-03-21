0076aa76ff2442cd438d0ab84450f988c7ba76b3
NMINHDAI <ngominhdai92tn@gmail.com>
2022-03-21 17:34:24 +0700

    A

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export var firebaseConfig = {
-    apiKey: "%apiKey%",
-    authDomain: "socialapp-61e2c.firebaseapp.com",
-    projectId: "socialapp-61e2c",
-    storageBucket: "socialapp-61e2c.appspot.com",
-    messagingSenderId: "227799603604",
-    appId: "1:227799603604:web:b9f26ed25cf2a57fb39781",
-    measurementId: "G-7MWS10K08P"
-  };
